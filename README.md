e6f175372b9f23b3cb297b12266b7f6f69f544e2
dlynch <dlynch@gopivotal.com>
2017-02-20 08:35:24 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pushing an app
+
+```
+cf push web1 -s windows2012R2 -b binary_buildpack -p .\WebApplication1\ --health-check-type none
+```
---
+++ WebApplication1/WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public string[] Get()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(WorkThreadFunction));
+            thread.Start();
+
+            return new string[]
+            {
+        "Hello",
+        "World"
+            };
+        }
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("CurrentDomain_UnhandledException:" + e.ToString());
+            Console.ReadKey();
+        }
+        public static void WorkThreadFunction()
+        {
+            throw new Exception("exception in CLR Thread and exit");
+        }
+
+    }
+}
